0c5e7950c74569a57fca0b0d8a5b4e21b73577ad
Tore Amundsen <tore@amundsen.org>
2020-12-16 18:26:15 +0100

    Update dhcp-mqtt-pub.sh

--- dhcp-mqtt-pub.sh
+++ dhcp-mqtt-pub.sh
@@ -1,26 +1,44 @@
 #!/usr/bin/env bash
 
-export DNSMASQ_ARG_OPERATION=$1
-export DNSMASQ_ARG_MAC=$2
-export DNSMASQ_ARG_IP=$3
-export DNSMASQ_ARG_HOSTNAME=$4
+# Set mosquitto_pub arguments in [scriptname].mosqcfg
+
+DEFUALT_TOPIC="dnsmasq/lease" # default topic if not set in mosqcfg
+
+# Get all arguments and create environment variables named by number starting from 1
+argument_count=$#
+argument_values=("$@")
+for (( x=1; x<=argument_count; x++ ))
+do
+    export DNSMASQ_SCRIPT_ARG_$x="${argument_values[x-1]}"
+done
 
 config_file="${BASH_SOURCE%.*}".mosqcfg
 if [ ! -e "$config_file" ]
 then
-  echo "Cannot find required config file: $config_file"
-  exit 1
+    # Read config file into array variable
+    mapfile -t pub_arguments < "$config_file"
 fi
 
-# Read config file into MAPFILE
-mapfile -t pub_arguments < "$config_file"
+# Iterate config to check for topic override and remove comment lines and empty lines
+config_has_topic=false
+cleaned_pub_arguments=()
+if [[ -v pub_arguments ]]
+then
+    for arg in "${pub_arguments}"
+    do
+        trimmed="${arg#"${arg%%[![:space:]]*}"}"   # remove leading whitespace characters
+        trimmed="${trimmed%"${trimmed##*[![:space:]]}"}"   # remove trailing whitespace characters
+        if [[ $trimmed =~ ^#.* ]]; then trimmed=""; fi # remove comment lines
+        if [[ $trimmed == "-t *" ]] || [[ $trimmed == "--topic *" ]]; then config_has_topic=true; fi
+        if [[ $trimmed == "-L *" ]] || [[ $trimmed == "--url *" ]]; then config_has_topic=true; fi
+        if [ ! -z "$trimmed" ]; then cleaned_pub_arguments+=("$trimmed"); fi
+    done
+fi
 
-# TODO: argumens
-# - remove only whitespace lines
-# - trim start and end
-# - trim comment lines
-# - check config_file not empty
-# - remove -m message argument
-# - remove set -t argument to default if missing
+# Set default topic if missing in config
+if [ "$config_has_topic" = false ]; then cleaned_pub_arguments+=("--topic '$DEFUALT_TOPIC'"); fi
 
-message=$(env | grep '^DNSMASQ_' | sort)
+# Build messages from all environment variables with names starting with DNSMASQ_
+message=$(env | grep '^DNSMASQ_')
+
+mosquitto_pub -m "$message" ${cleaned_pub_arguments[@]}
